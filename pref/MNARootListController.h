cc3f837ccc89540e9f1493c4d4dab7a0890a9182
Hao Nguyen <hao.ict56@gmail.com>
2020-04-20 05:08:18 +0900

    Refine Pref

--- pref/MNARootListController.h
+++ pref/MNARootListController.h
@@ -2,33 +2,11 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 
-@interface NSTask : NSObject
-@property (copy) NSArray *arguments;
-@property (copy) NSString *currentDirectoryPath;
-@property (copy) NSDictionary *environment;
-@property (copy) NSString *launchPath;
-@property (readonly) int processIdentifier;
-@property (retain) id standardError;
-@property (retain) id standardInput;
-@property (retain) id standardOutput;
-+ (id)currentTaskDictionary;
-+ (id)launchedTaskWithDictionary:(id)arg1;
-+ (id)launchedTaskWithLaunchPath:(id)arg1 arguments:(id)arg2;
-- (id)init;
-- (void)interrupt;
-- (bool)isRunning;
-- (void)launch;
-- (bool)resume;
-- (bool)suspend;
-- (void)terminate;
-@end
-
-@interface MNAHeader : UITableViewCell {
-  UILabel *label;
-  UILabel *underLabel;
-}
-@end
+#import "HaoPSUtil.h"
+#import "HaoPSHeaderImageCellCustom.h"
+#import "HaoPSHeaderCellCustom.h"
+#import "HaoPSLinkCellCustom.h"
+#import "HaoPSSwitchCellCustom.h"
 
 @interface MNARootListController : PSListController
-+ (id)sharedInstance;
 @end
--- pref/MNARootListController.m
+++ pref/MNARootListController.m
@@ -1,92 +1,45 @@
 #include "MNARootListController.h"
 
-#define TWEAK_TITLE "Messenger No Ads"
 #define PLIST_PATH "/var/mobile/Library/Preferences/com.haoict.messengernoadspref.plist"
-#define PREF_BUNDLE_PATH "/Library/PreferenceBundles/MNAPref.bundle"
 #define PREF_CHANGED_NOTIF "com.haoict.messengernoadspref/PrefChanged"
-#define kTintColor [UIColor colorWithRed:0.72 green:0.53 blue:1.00 alpha:1.00];
 
-/**
- * Header, logo
- */
-@implementation MNAHeader
-- (id)initWithSpecifier:(PSSpecifier *)specifier {
-  self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
-  if (self) {
-    #define kWidth [[UIApplication sharedApplication] keyWindow].frame.size.width
-    NSArray *subtitles = [NSArray arrayWithObjects:@"By @haoict", @"Free and Open Source!", nil];
-
-    CGRect labelFrame = CGRectMake(0, -15, kWidth, 80);
-    CGRect underLabelFrame = CGRectMake(0, 35, kWidth, 60);
-
-    label = [[UILabel alloc] initWithFrame:labelFrame];
-    [label setNumberOfLines:1];
-    label.font = [UIFont systemFontOfSize:35];
-    [label setText:@TWEAK_TITLE];
-    label.textColor = kTintColor;
-    label.textAlignment = NSTextAlignmentCenter;
-
-    underLabel = [[UILabel alloc] initWithFrame:underLabelFrame];
-    [underLabel setNumberOfLines:1];
-    underLabel.font = [UIFont systemFontOfSize:15];
-    uint32_t rnd = arc4random_uniform([subtitles count]);
-    [underLabel setText:[subtitles objectAtIndex:rnd]];
-    underLabel.textColor = [UIColor grayColor];
-    underLabel.textAlignment = NSTextAlignmentCenter;
-
-    [self addSubview:label];
-    [self addSubview:underLabel];
-  }
-  return self;
-}
-- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 {
-  CGFloat prefHeight = 75.0;
-  return prefHeight;
-}
-@end
-
-/**
- * Root: No change if not needed
- */
 @implementation MNARootListController
-MNARootListController *sharedInstance;
-+ (id)sharedInstance {
-  return sharedInstance;
-}
-+ (NSString *)localizedItem:(NSString *)key {
-  NSBundle *tweakBundle = [NSBundle bundleWithPath:@PREF_BUNDLE_PATH];
-  return [tweakBundle localizedStringForKey:key value:@"" table:@"Root"] ?: @"";
-}
 - (id)init {
   self = [super init];
   if (self) {
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:[MNARootListController localizedItem:@"APPLY"] style:UIBarButtonItemStylePlain target:self action:@selector(apply)];;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:[HaoPSUtil localizedItem:@"APPLY"] style:UIBarButtonItemStylePlain target:self action:@selector(apply)];;
   }
-  sharedInstance = self;
   return self;
 }
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
+  // set switches color
   UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
   self.view.tintColor = kTintColor;
   keyWindow.tintColor = kTintColor;
   [UISwitch appearanceWhenContainedInInstancesOfClasses:@[self.class]].onTintColor = kTintColor;
+  // set navigation bar color
+  self.navigationController.navigationController.navigationBar.barTintColor = kTintColor;
+  self.navigationController.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+  [self.navigationController.navigationController.navigationBar setShadowImage: [UIImage new]];
+}
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  [self.navigationController.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
 }
 - (void)viewWillDisappear:(BOOL)animated {
   [super viewWillDisappear:animated];
   UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
   keyWindow.tintColor = nil;
+  self.navigationController.navigationController.navigationBar.barTintColor = nil;
+  self.navigationController.navigationController.navigationBar.tintColor = nil;
+  [self.navigationController.navigationController.navigationBar setShadowImage:nil];
+  [self.navigationController.navigationController.navigationBar setTitleTextAttributes:nil];
 }
+
 - (NSArray *)specifiers {
   if (!_specifiers) {
-    _specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
-  }
-  /** Localize sublabel **/
-  for (PSSpecifier *spec in _specifiers) {
-    NSString *sublabelValue = [spec.properties objectForKey:@"sublabel"];
-    if (sublabelValue) {
-      [spec setProperty:[MNARootListController localizedItem:sublabelValue] forKey:@"sublabel"];
-    }
+    _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
   }
 
   /** Disable Hide Search Bar for ios 13 - begin **/
@@ -126,15 +79,15 @@ MNARootListController *sharedInstance;
  * Apply top right button
  */
 -(void)apply {
-  UIAlertController *killConfirm = [UIAlertController alertControllerWithTitle:@TWEAK_TITLE message:[MNARootListController localizedItem:@"DO_YOU_REALLY_WANT_TO_KILL_MESSENGER"] preferredStyle:UIAlertControllerStyleAlert];
-  UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:[MNARootListController localizedItem:@"CONFIRM"] style:UIAlertActionStyleDestructive handler:^(UIAlertAction * action) {
+  UIAlertController *killConfirm = [UIAlertController alertControllerWithTitle:@TWEAK_TITLE message:[HaoPSUtil localizedItem:@"DO_YOU_REALLY_WANT_TO_KILL_MESSENGER"] preferredStyle:UIAlertControllerStyleAlert];
+  UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:[HaoPSUtil localizedItem:@"CONFIRM"] style:UIAlertActionStyleDestructive handler:^(UIAlertAction * action) {
     NSTask *killall = [[NSTask alloc] init];
     [killall setLaunchPath:@"/usr/bin/killall"];
     [killall setArguments:@[@"-9", @"Messenger", @"LightSpeedApp"]];
     [killall launch];
   }];
 
-  UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:[MNARootListController localizedItem:@"CANCEL"] style:UIAlertActionStyleCancel handler:nil];
+  UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:[HaoPSUtil localizedItem:@"CANCEL"] style:UIAlertActionStyleCancel handler:nil];
   [killConfirm addAction:confirmAction];
   [killConfirm addAction:cancelAction];
   [self presentViewController:killConfirm animated:YES completion:nil];
